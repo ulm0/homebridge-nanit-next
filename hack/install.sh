9fa84dcfd83d3c865f71753bcd0f3cb5b6ac5266
Pierre Ugaz <me@ulm0.com>
2026-03-16 10:37:09 -0300

    chore(ci): ci and publish

--- hack/install.sh
+++
@@ -1,8 +0,0 @@
-npm install --prefix /homebridge --ignore-scripts homebridge-nanit-1.3.11.tgz && \
-node -e " \
-        const fs = require(\"fs\"); \
-        const pkg = JSON.parse(fs.readFileSync(\"/homebridge/package.json\",\"utf8\")); \
-        const ver = require(\"/homebridge/node_modules/homebridge-nanit/package.json\").version; \
-        pkg.dependencies[\"homebridge-nanit\"] = ver; \
-        fs.writeFileSync(\"/homebridge/package.json\", JSON.stringify(pkg, null, 2)); \
-"
